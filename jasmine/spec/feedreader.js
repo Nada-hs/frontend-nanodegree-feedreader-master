c3b1115b4c5bfd03230866fd327b51d8a684a4b5
Nada-hs <56253677+Nada-hs@users.noreply.github.com>
2019-11-08 11:57:19 +0300

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,35 +53,27 @@ $(function() {
    TODO:/* Write a new test suite named "The menu" */
 
     describe('The menu', function() {
-
-        TODO: /* Write a test that ensures the menu element is
+        /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        
-        it('Hidden by default',function(){
-            let Hidden = document.body.classList.contains('menu-hidden');
-            expect (Hidden).toBe(true);
+        it('is hidden', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+            // ref: https://api.jquery.com/hasclass/
         });
 
-
-        TODO: /*  Write a test that ensures the menu changes
+         /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-    
-            it('Toggle when menu icon clicked',function(){
+         it('toggles visibility on click', function() {
             $('a.menu-icon-link').trigger('click'); // show menu
-            expect($('body').hasClass('menu-hidden')).toBe(true);
-            $('a.menu-icon-link').trigger('click'); // hide menu again
             expect($('body').hasClass('menu-hidden')).toBe(false);
-
-        });
-
-
-
+            $('a.menu-icon-link').trigger('click'); // hide menu again
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
     });
   
 
